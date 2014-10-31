ddf646c9870d2e196d957d5926f24348ef8af6bc
ALICE <turgus.cassie@gmail.com>
2014-10-31 18:02:13 -0500

    Bunch of stuff

--- OOP/src/ScholarshipSystem/Admin.java
+++ OOP/src/ScholarshipSystem/Admin.java
@@ -5,13 +5,16 @@ import java.util.*;
 /**
  * Created by alice on 10/22/14.
  */
+
+/*
+ TODO: Potentially update call to Scholarship constructor when deadline stuff is figured out
+ */
+
 public class Admin {
 
 	private int adminID;
 	private String password;
 
-	// java provides default constructor that does exactly this;
-	// good practice to be explicit with it?
 	public Admin()
 	{
 		adminID = -1;
@@ -31,28 +34,35 @@ public class Admin {
 
 	public void addScholarship()
 	{
-		// I/O skeleton just to show what info will be needed from admin.
-		
-		// TODO: Create new Scholarship object with provided data: need Scholarship constructor(s) to be written.
-		
-		// TODO: Create Criteria object for scholarship requirements -
-		// pass GPA, major, hours to Scholarship for it to pass to
-		// Criteria? Need Criteria constructor(s) to be written.
+		// I/O for now
 
 		Scanner stdin = new Scanner(System.in);
 
-		System.out.println("Enter the scholarship name: ");
+		System.out.println("Scholarship name: ");
 		String name = stdin.nextLine();
 
-		System.out.println("Enter the scholarship amount: ");
-		double amount = stdin.nextDouble();
+		System.out.println("Award amount: ");
+		int amount = stdin.nextInt();
 
-		System.out.println("Enter the maximum number of students: ");
+        System.out.println("Application deadline: ");
+        String deadline = stdin.nextLine();
+
+		System.out.println("Maximum number of awardees: ");
 		int studentLimit = stdin.nextInt();
 
-		System.out.println("Enter deadline: ");
-		String deadline = stdin.nextString();
+        System.out.println("Required major: "); // expand to multiple allowed majors in future iterations?
+        String major = stdin.nextLine();
 
+        System.out.println("Minimum required credit hours: ");
+        int hours = stdin.nextInt();
+
+        System.out.println("Minimum required GPA: ");
+        double gpa = stdin.nextDouble();
+
+        Scholarship new_scholarship = new Scholarship(amount, name, deadline, studentLimit, major, hours, gpa);
+
+        System.out.println("Scholarship successfully created!");
+        // print toString of new scholarship
 	}
 
 }
--- OOP/src/ScholarshipSystem/Student.java
+++ OOP/src/ScholarshipSystem/Student.java
@@ -3,6 +3,12 @@ package ScholarshipSystem;
 /**
  * Created by alice on 10/22/14.
  */
+
+/*
+TODO: Overload toString()
+TODO: Overload equals ?
+ */
+
 public class Student {
 
 	private int studentID;
@@ -13,15 +19,13 @@ public class Student {
 	{
 		studentID = -1;
 		password = "";
-		studentCrit = null; // does this do in java what I want it to do as if this is C++?
+		studentCrit = null;
 	}
-	public Student(int id, String pw, double gpa, String major, int hours)
+	public Student(int id, String pw, String major, int hours, double gpa)
 	{
 		studentID = id;
 		password = pw;
-		// studentCrit = new Criteria( // pass gpa major & hours )
-		// need Criteria constructor(s) to be written
-		// other option is just to pass Criteria object and assign to studentCrit but I think this is better...data hiding? that might be what this is
+		//studentCrit = new Criteria(major, hours, gpa); // need Criteria constructor
 	}
 
 }
